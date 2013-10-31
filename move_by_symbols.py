d7e024777ca162fa1dce7fcd7565a237d4965f12
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-11-01 03:05:12 +0400

    use settings in case of missing arguments

--- move_by_symbols.py
+++ move_by_symbols.py
@@ -6,13 +6,41 @@ __author__ = 'Eldar Abusalimov'
 
 import sublime_plugin
 
+
 class MoveBySymbolsCommand(sublime_plugin.TextCommand):
 
-    def run(self, edit, forward=None, single_selection=False,
-            symbol_selector='entity.name'):
-        if forward is None:
-            return  # to bypass TypeError on missing argument
+    def run(self, edit, **kwargs):
+        try:
+            forward = bool(kwargs['forward'])
+        except KeyError:
+            return  # do nothing in case of missing required argument
 
+        settings = self.view.settings()
+
+        # By default one can move multiple selections (why not, after all?).
+        # Setting 'single_selection' to true discards all selections
+        # except the first/last (depending on the direction used).
+        try:
+            single_selection = kwargs['single_selection']
+        except KeyError:
+            single_selection = settings.get('single_selection', False)
+
+        # Some syntax bundles (like Python) override default symbol selector
+        # to get more neat looking symbol outline.
+        # For example, for this file the first two symbols from outline are
+        # 'class MoveBySymbolsCommand(sublime_plugin.TextCommand):' and
+        # '    def run(...):'.
+        # However, it is much more convenient to navigate by selecting
+        # identifiers only, like 'MoveBySymbolsCommand' and 'run', without a
+        # surrounding text.
+        try:
+            symbol_selector = kwargs['symbol_selector']
+        except KeyError:
+            symbol_selector = settings.get('symbol_selector', 'entity.name')
+
+        self.do_move(forward, single_selection, symbol_selector)
+
+    def do_move(self, forward, single_selection, symbol_selector):
         sel = self.view.sel()
 
         if single_selection:
